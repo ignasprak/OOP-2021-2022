f4dbf0c90e9734131fe5d58534e39f53fe80cf8c
ignasprak <iamgpopa@gmail.com>
2022-01-31 16:47:20 +0000

    bla

--- java/src/ie/tudublin/Cat.java
+++ java/src/ie/tudublin/Cat.java
@@ -1,9 +1,37 @@
 package ie.tudublin;
 
-public class Cat extends Animal
+public class Cat extends Animal //Cat class being inherited from the animal class
 {
+    int numLives; //initialising variable
+
+    public int getNumLives() //when a method is public, that means it can be accessed by other objects
+    {
+        return this.numLives; //why do we use this and return here?
+    }
+
+    public void setNumlives(int numLives)
+    {
+        this.numLives = numLives;
+    }
+
     public Cat(String name)
     {
         super(name);
+        numLives = 9;
     }
+
+    public void Kill() //void cannot return values
+    {
+        // return numLives;
+        if (this.numLives <= 0)
+        {
+            System.out.println("dead X-X");
+        }
+        else
+        {
+            System.out.println("ouchie! 0-0");
+            this.numLives -= 1;
+        }
+    }
+
 }
--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -2,13 +2,28 @@ package ie.tudublin;
 
 public class Main
 {
-    public static void helloProcessing()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new HelloProcessing());
-    }
     public static void main(String[] args)
     {
-        helloProcessing();
+        System.out.println("Hello world");
+
+        Animal misty = new Animal("Misty");
+        Animal lucy = new Animal("Lucy");
+
+        lucy = misty;
+        misty.setName("Tara");
+
+        System.out.println(misty);
+        System.out.println(lucy);
+
+        // Tara Misty
+        // Tara Tara
+        
+        // below we have setup the class call?
+        Cat ginger = new Cat("Ginger");
+        for(int i = 0; i < 10; i++)
+        {
+            ginger.Kill();
+        }
+        
     }
 }
